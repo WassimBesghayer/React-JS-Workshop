d86c00bf11882b105493eeec4efecff19be5d061
Wassim <wassim.besghayer@gmail.com>
2025-04-15 17:35:12 +0100

    re-structuring

---
+++ src/components/data.jsx
@@ -0,0 +1,7 @@
+var animal = {
+  kind: "Cat",
+  breed: "Turkish",
+  photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4k2FWyME_9j9enhK8eDAjCCSSRCjQzADumQ&s",
+}
+
+export default data
