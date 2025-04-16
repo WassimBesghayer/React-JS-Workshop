5ed72a321a2410f30bf8f27318d79f798c7f4a76
Wassim <wassim.besghayer@gmail.com>
2025-04-16 15:21:09 +0100

    v.1

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -2,6 +2,7 @@ var animal = {
   kind: "Cat",
   breed: "Turkish",
   photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4k2FWyME_9j9enhK8eDAjCCSSRCjQzADumQ&s",
+  description: "L'Angora turc est une race de chats à poils mi-longs originaire de Turquie. Ce chat est caractérisé par sa robe à poils mi-longs très soyeuse et sans sous-poils, et une silhouette fine et élancée qui le classe dans la catégorie des chats foreign comme l'abyssin ou le bleu russe.",
 }
 
-export default data
+export default animal
